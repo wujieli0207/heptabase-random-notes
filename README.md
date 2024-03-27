9c833c140c64acd25c42e98b1101d5334860b386
wujieli0207 <wujieli0207@outlook.com>
2024-03-27 19:24:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Heptabase random notes
+
+一个 chrome 插件，支持随机翻阅 Heptabase 的笔记
---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@radix-ui/themes'
+
+function App() {
+  return (
+    <>
+      <Button>test</Button>
+    </>
+  )
+}
+
+export default App
